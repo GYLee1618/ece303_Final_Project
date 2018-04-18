70db8f45379bdb78458f10fc3892b70f800624df
Karol <karolw155@gmail.com>
2018-04-18 15:43:00 -0400

    Added pseudocode and function templates

--- 2018/receiver.py
+++ 2018/receiver.py
@@ -1,7 +1,4 @@
-# Written by S. Mevawala, modified by D. Gitzel
-
 import channelsimulator
-
 
 class Receiver(object):
 
@@ -14,6 +11,19 @@ class Receiver(object):
 
     def receive(self):
         raise NotImplementedError("The base API class has no implementation. Please override and add your own.")
+
+    def validPacket():	#Checks the checksum of the packet to make sure the data wasn't corrupted.
+
+    def genCheck():		#Generates a checksum for the acknowledgement message.
+
+    def sendMessage(seqNum):	#Send a message to sender saying which packet which packet it wants next. Will call genCheck to make checkSum.
+
+    # First take incoming packets and use validPacket(). Return a value 0 for good packet and -1 for bad packet.
+    	#If good packet mark that it has been received in receied vector array.
+    	#If bad packet don't mark it in the received vector array.
+    #Check received vector for the value of first unreceived packet.
+    #Use sendMessage to send a request for the the next unreceived packet number (seqNum).
+
 
 
 class BogoReceiver(Receiver):
