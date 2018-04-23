b93c57575df8fb579147f29efa4ff7984bc45755
GYLee1618 <33380668+GYLee1618@users.noreply.github.com>
2018-04-23 09:33:15 -0400

    added makepkt function

--- 2018/checksum.py
+++ 2018/checksum.py
@@ -7,3 +7,17 @@ def fletch_sum(data):
 		b = (b + a) % 65536
 		i += 16
 	return (b << 16) | a
+
+#makes the packet given binary sequence number and data (expected to be binary strings)
+#checksum used is 32-bit Fletcher checksum
+#returns binary packet (binary string)
+def makepkt(data,seqnum):
+	sn = seqnum
+	chksum = bin(fletch_sum(data))
+	packet = '0b'
+
+	packet = packet + sn[2:len(sn)]
+	packet = packet + data[2:len(data)]
+	packet = packet + chksum[2:len(chksum)]
+
+	return packet
